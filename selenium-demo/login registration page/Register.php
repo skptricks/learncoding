88c2c2ab60bf531be2adf3c3f2a5458838ec54dc
skptricks <sumit.pradhan96@gmail.com>
2018-04-01 20:18:53 +0530

    updated

---
+++ selenium-demo/login registration page/Register.php
@@ -0,0 +1,31 @@
+
+<html >
+<head>
+<link rel="stylesheet" type="text/css" href="design.css">
+</head>
+
+
+<body>
+
+
+
+<div>
+<a href="login.php">Login</a>
+</div>
+
+<div id="register">
+<h3>Registration page</h3>
+<form method="post" action="" name="register">
+<label>Name</label>
+<input type="text" name="username" autocomplete="off" />
+<label>Email</label>
+<input type="text" name="emailid" autocomplete="off" />
+<label>Password</label>
+<input type="password" name="userpassowrd" autocomplete="off"/>
+<input type="submit" class="button" name="submitregistrationform" value="Register">
+</form>
+</div>
+
+</body>
+
+</html>
